59988b831571899523951a8394304bb1dffa7e8a
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-03-11 07:42:33 +0200

    Update example with protocol negotiation

--- example/client.php
+++ example/client.php
@@ -5,8 +5,10 @@ use Onion\Framework\WebSocket\Frame;
 use Onion\Framework\WebSocket\Stream;
 require __DIR__ . '/../vendor/autoload.php';
 
-$client = new Client('http://localhost:1337', 5);
-$socket = $client->connect()->then(function (Stream $socket) {
+$client = new Client('http://localhost:1337', 10);
+$socket = $client->connect([
+    'Sec-WebSocket-Protocol' => 'soap',
+])->then(function (Stream $socket) {
     $socket->write(new Frame((string) time()), true);
     echo "> {$socket->read()}";
     $socket->close();
