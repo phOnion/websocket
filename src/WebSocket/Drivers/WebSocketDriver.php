f52a197534f4b2ad2e7cbb4e3bfe2d4b5f273cf0
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-09-30 09:01:02 +0300

    improveme driver handling for multiple clients

--- src/WebSocket/Drivers/WebSocketDriver.php
+++ src/WebSocket/Drivers/WebSocketDriver.php
@@ -3,6 +3,8 @@ namespace Onion\Framework\WebSocket\Drivers;
 
 use function Onion\Framework\Http\build_request;
 use Onion\Framework\Http\Events\RequestEvent;
+use Onion\Framework\Loop\Coroutine;
+use Onion\Framework\Loop\Interfaces\ResourceInterface;
 use Onion\Framework\Server\Drivers\DriverTrait;
 use Onion\Framework\Server\Interfaces\ContextInterface;
 use Onion\Framework\Server\Interfaces\DriverInterface;
@@ -32,32 +34,33 @@ class WebSocketDriver implements DriverInterface
         $socket = self::createSocket($address, $port, $contexts);
         while ($socket->isAlive()) {
             yield $socket->wait();
-            try {
-                $connection = $socket->accept();
-            } catch (\InvalidArgumentException $ex) {
-                // Accept failed, we ok
-                continue;
-            }
+            $connection = yield $socket->accept();
 
             yield $connection->wait();
-            $data = '';
-            while ($connection->isAlive()) {
-                $chunk = $connection->read(8192);
-                if (!$chunk) {
-                    break;
+            yield Coroutine::create(function (ResourceInterface $connection, EventDispatcherInterface $dispatcher) {
+                $data = '';
+                while ($connection->isAlive()) {
+                    $chunk = $connection->read(8192);
+                    if (!$chunk) {
+                        break;
+                    }
+
+                    yield $data .= $chunk;
                 }
 
-                $data .= $chunk;
-            }
-
-            $request = build_request($data);
-            if ($request->hasHeader('upgrade') && $request->getHeaderLine('upgrade') === 'websocket') {
-                yield $this->dispatcher->dispatch(new HandshakeEvent($request, $connection,));
-            } else {
-                yield $this->dispatcher->dispatch(
-                    new RequestEvent($request, $connection)
-                );
-            }
+                try {
+                    $request = build_request($data);
+                    if ($request->hasHeader('upgrade') && $request->getHeaderLine('upgrade') === 'websocket') {
+                        yield $dispatcher->dispatch(new HandshakeEvent($request, $connection,));
+                    } else {
+                        yield $dispatcher->dispatch(
+                            new RequestEvent($request, $connection)
+                        );
+                    }
+                } catch (\InvalidArgumentException $ex) {
+                    // Accept failed, we ok
+                }
+            }, [$connection, $this->dispatcher]);
         }
     }
 }
