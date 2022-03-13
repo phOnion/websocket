2a0f1f9a4b8c4455f608575eecb474cd509f67b3
Dimitar Dimitrov <daghostman.dd@gmail.com>
2022-03-13 16:50:35 +0200

    overall improvements to frame encoding & decoding

--- src/WebSocket/Frame.php
+++ src/WebSocket/Frame.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Onion\Framework\WebSocket;
 
 use Onion\Framework\Loop\Interfaces\ResourceInterface;
@@ -7,33 +9,35 @@ use Onion\Framework\WebSocket\Types\{Types, CloseReasons, Flags};
 use RuntimeException;
 use Stringable;
 
-use function Onion\Framework\Loop\tick;
-
 class Frame implements Stringable
 {
     private const OPCODE_MASKED = 0b10000000;
 
     public function __construct(
-        private readonly ?string $data = null,
-        private readonly Types $opcode = Types::TEXT,
+        private readonly Types $type,
+        private readonly bool $masked,
         private readonly bool $final = true,
-        private readonly bool $masked = false,
+        private readonly string $data = '',
+        public readonly bool $reserved1 = false,
+        public readonly bool $reserved2 = false,
+        public readonly bool $reserved3 = false,
+        public readonly int $length = 0,
     ) {
     }
 
     public function getOpcode(): Types
     {
-        return $this->opcode;
+        return $this->type;
     }
 
     public function isFinal(): bool
     {
-        return $this->final === true;
+        return $this->final;
     }
 
     public function getData(): string
     {
-        return (string) $this->data;
+        return $this->data;
     }
 
     public function isMasked(): bool
@@ -41,100 +45,129 @@ class Frame implements Stringable
         return $this->masked;
     }
 
+    public function getLength(): int
+    {
+        return $this->length;
+    }
+
+    public function withData(string $data): static
+    {
+        return new Frame(
+            $this->getOpcode(),
+            $this->isMasked(),
+            $this->isFinal(),
+            $data,
+            $this->reserved1,
+            $this->reserved2,
+            $this->reserved3,
+            strlen($data),
+        );
+    }
+
     public function __toString()
     {
         return $this->getData();
+    }
+
+    public static function create(
+        array $header,
+        ?string $data = '',
+    ): Frame {
+        return new Frame(
+            type: Types::tryFrom($header[0] & Flags::OPCODE->value),
+            masked: ($header[1] | static::OPCODE_MASKED) === $header[1],
+            final: ($header[0] | Flags::FINISHED->value) === $header[0],
+            reserved1: ($header[1] & Flags::RESERVED1->value) === Flags::RESERVED1->value,
+            reserved2: ($header[1] & Flags::RESERVED2->value) === Flags::RESERVED2->value,
+            reserved3: ($header[1] & Flags::RESERVED3->value) === Flags::RESERVED3->value,
+            data: $data,
+            length: (int) ($header[1] & Flags::LENGTH->value),
+        );
     }
 
     public static function encode(Frame $frame): string
     {
         $data = $frame->getData();
         $length = strlen($data);
-        $header = chr(($frame->isFinal() ? 0x80 : 0) | $frame->getOpcode()->value);
+        $header = chr(($frame->isFinal() ? Flags::FINISHED->value : 0) | $frame->getOpcode()->value);
 
+        $mask = $frame->isMasked() ? static::OPCODE_MASKED : 0b100000000;
         // Mask 0x80 | payload length (0-125)
-        if ($length < 126) $header .= chr(0x80 | $length);
-        elseif ($length < 0xFFFF) $header .= chr(0x80 | 126) . pack("n", $length);
-        elseif (PHP_INT_SIZE > 4) // 64 bit
-            $header .= chr(0x80 | 127) . pack("Q", $length);
-        else  // 32 bit (pack Q dosen't work)
-            $header .= chr(0x80 | 127) . pack("N", 0) . pack("N", $length);
+        if ($length < 126) {
+            $header .= chr($mask | $length);
+        } elseif ($length < 0xFFFF) {
+            $header .= chr($mask | 126) . pack("n", $length);
+        } elseif (PHP_INT_SIZE > 4) { // 64 bit
+            $header .= chr($mask | 127) . pack("Q", $length);
+        } else { // 32 bit (pack Q dosen't work)
+            $header .= chr($mask | 127) . pack("N", 0) . pack("N", $length);
+        }
 
-        // Add mask
-        $mask = pack("N", random_bytes(4));
-        $header .= $mask;
+        if ($frame->isMasked()) {
+            // Add mask
+            $mask = pack("N", random_bytes(4));
+            $header .= $mask;
 
-        // Mask application data.
-        for ($i = 0; $i < $length; $i++)
-            $data[$i] = chr(ord($data[$i]) ^ ord($mask[$i % 4]));
+            // Mask application data.
+            for ($i = 0; $i < $length; $i++) {
+                $data[$i] = chr(ord($data[$i]) ^ ord($mask[$i % 4]));
+            }
+        }
 
         return $header . $data;
     }
 
     public static function decode(ResourceInterface $resource): ?Frame
     {
-        $byte = [Types::TEXT->value];
-        $contents = '';
-        $finished = true;
-        $masked = false;
+        $header = $resource->read(2);
+        if (strlen($header) !== 2) {
+            return null;
+        }
 
-        do {
-            $h = $resource->read(2);
-            if (strlen($h) !== 2) {
-                return null;
-            }
+        $byte = [ord($header[0]), ord($header[1])];
+        $frame = static::create($byte);
 
-            $header = [$h[0], $h[1]];
+        $length = $frame->getLength();
 
-            $byte = [ord($header[0]), ord($header[1])];
-            $finished = ($byte[0] | Flags::FINISHED->value) === $byte[0];
-            $masked = ($byte[1] & static::OPCODE_MASKED) ? true : false;
-            $length = (int) $byte[1] & Flags::LENGTH->value;
+        if ($length === 126) {
+            $length = unpack('n', $resource->read(2), 0)[1];
+        } elseif ($length === 127) {
+            $lp = \unpack('N2', $resource->read(8))[0];
 
-            if ($length === 126) {
-                $length = unpack('n', $resource->read(2), 0)[1];
-            } elseif ($length === 127) {
-                $lp = \unpack('N2', $resource->read(8))[0];
-
-                if (PHP_INT_MAX === 0x7FFFFFFF) {
-                    if ($lp[1] !== 0 || $lp[2] < 0) {
-                        throw new \OutOfBoundsException(
-                            'Max payload size exceeded',
-                            CloseReasons::MESSAGE_TOO_LONG->value
-                        );
-                    }
-                    $length = $lp[2];
-                } else {
-                    $length = $lp[1] << 32 | $lp[2];
+            if (PHP_INT_MAX === 0x7FFFFFFF) {
+                if ($lp[1] !== 0 || $lp[2] < 0) {
+                    throw new \OutOfBoundsException(
+                        'Max payload size exceeded',
+                        CloseReasons::MESSAGE_TOO_LONG->value
+                    );
                 }
-            }
-
-            if ($length < 0) {
-                throw new RuntimeException(
-                    'Payload length resulted in negative',
-                    CloseReasons::INVALID_FRAME_DATA->value
-                );
-            }
-
-            if ($masked) {
-                $mask = $resource->read(4);
-                for ($i = 0; $i < $length; $i++) {
-                    $ch = $resource->read(1);
-                    $contents[$i] = $ch ^ $mask[$i % 4];
-                }
+                $length = $lp[2];
             } else {
-                $contents .= $length ? $resource->read($length) : '';
+                $length = $lp[1] << 32 | $lp[2];
             }
+        }
 
-            tick();
-        } while (!$finished);
+        if ($length < 0) {
+            throw new RuntimeException(
+                'Payload length resulted in negative',
+                CloseReasons::INVALID_FRAME_DATA->value
+            );
+        }
 
-        return new Frame(
-            $contents,
-            Types::from($byte[0] & Flags::OPCODE->value),
-            $finished,
-            $masked,
-        );
+        $contents = '';
+        if ($frame->isMasked()) {
+            $mask = $resource->read(4);
+            for ($i = 0; $i < $length; $i++) {
+                $ch = $resource->read(1);
+                $contents[$i] = $ch ^ $mask[$i % 4];
+            }
+        } else {
+            while (strlen($contents) < $length) {
+                $contents .= $resource->read($length - strlen($contents));
+            }
+        }
+
+        return $frame->withData($contents);
     }
 
     public function __debugInfo()
